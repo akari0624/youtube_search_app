4428236f1510b5aec28c89f6ef074ee43eba5b9e
morris.chen <morris.chen@johnsontechinc.com>
2020-08-13 18:23:35 +0800

    use useSWR to fetch and cache data

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,10 +1,39 @@
 import swal from 'sweetalert2'
 
-const getYOUTUBE_DATA_V3_URL = (searchKeyWords, apiKey, dataCountPerPage = 12) =>
-  `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchKeyWords}&chart=mostPopular&maxResults=${dataCountPerPage}&key=${process.env.REACT_APP_YOUTUBE_API_TOKEN}`;
+export const getYOUTUBE_DATA_V3_URL = (searchKeyWords, dataCountPerPage = 12) => {
+  if(!searchKeyWords) {
+    return null
+  }
+  return `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchKeyWords}&chart=mostPopular&maxResults=${dataCountPerPage}&key=${process.env.REACT_APP_YOUTUBE_API_TOKEN}`;
+}
 
 export const youtube_query = (searchKeyWords) => {
   const url = getYOUTUBE_DATA_V3_URL(searchKeyWords, 'AIzaSyCCMyURuR3ue5XCRa4qY2q_Ud3sSEKhhOg')
+  return fetch(url).then(response => {
+    return response.json();
+  })
+  .then(result => {
+    console.log(result);
+    if(result.error) {
+      swal.fire({
+      icon: 'error',
+			titleText: '發生錯誤',
+			text: `code:${result.error.code}`
+		})
+    } else {
+    return result
+    }
+  }).catch(err => {
+    swal.fire({
+      icon: 'error',
+			titleText: '網路連線發生錯誤',
+			text: err.message
+		})
+  });
+};
+
+
+export const youtube_querySWRFetcher = (url) => {
   return fetch(url).then(response => {
     return response.json();
   })
--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,41 +1,56 @@
-import React, {useState} from 'react'
-import PropTypes from 'prop-types'
-import InputSearchBarForm from './components/InputSearchBarForm'
-import VideoCardsLayout from './components/VideoCardsLayout'
-import VideoCardItem from './components/VideoCardItem'
-import { youtube_query } from 'apis' 
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import InputSearchBarForm from "./components/InputSearchBarForm";
+import VideoCardsLayout from "./components/VideoCardsLayout";
+import VideoCardItem from "./components/VideoCardItem";
+import { youtube_query, getYOUTUBE_DATA_V3_URL, youtube_querySWRFetcher } from "apis";
+import useSWR from 'swr'
 
-function MainPage(props) {
-
-  const [currQueryResults, setCurrQueryResults] = useState([])
-  const [pageInfo, setPageInfo] = useState({
+function MainPage({searchKeyWords, setSearchKeyWords}) {
+  // const [currQueryResults, setCurrQueryResults] = useState([]);
+  // const [pageInfo, setPageInfo] = useState({
+  //   totalResults: 0,
+  //   resultsPerPage: 12,
+  // });
+  const {data: result, error} = useSWR(getYOUTUBE_DATA_V3_URL(searchKeyWords), youtube_querySWRFetcher )
+  // const onSubmit = async (values) => {
+  //   const result = await youtube_query(values.keywords);
+  //   console.log("the result", result);
+  //   if (result) {
+  //     setPageInfo((prev) => result.pageInfo);
+  //     setCurrQueryResults((prev) => result.items);
+  //   }
+  // };
+  const pageInfo = result?.pageInfo ?? {
     totalResults: 0,
     resultsPerPage: 12,
-  })
-  const onSubmit = async (values) => {
-    const result = await youtube_query(values.keywords)
-    console.log('the result', result)
-    if(result) {
-    setPageInfo(prev => result.pageInfo)
-    setCurrQueryResults(prev => result.items)
-    }
+  }
+  const currQueryResults = result?.items ?? [] 
+
+  const onSubmit = (values) => {
+    setSearchKeyWords(prev => values.keywords)
   }
 
   return (
     <div>
       <InputSearchBarForm placeholder="搜尋" onSubmit={onSubmit} />
       <VideoCardsLayout>
-        {
-          currQueryResults.map(item => <VideoCardItem key={item.id.videoId} item={item} />)
-        }
+        {currQueryResults.map((item) => (
+          <VideoCardItem key={item.id.videoId} item={item} />
+        ))}
       </VideoCardsLayout>
     </div>
+  );
+}
+
+MainPage.propTypes = {};
+
+
+const MainPageEnhancer = () => {
+  const [searchKeyWords, setSearchKeyWords] = useState(null)
+  return (
+  <MainPage searchKeyWords={searchKeyWords} setSearchKeyWords={setSearchKeyWords} />
   )
 }
 
-MainPage.propTypes = {
-
-}
-
-export default MainPage
-
+export default MainPageEnhancer;
