62b3cce15edb64dea9ad24c6ee71e419b8f482b2
morris.chen <morris.chen@johnsontechinc.com>
2020-09-08 13:41:03 +0800

    Updates

---
+++ precache-manifest.cd926b6bcbef28f6bf09c90f11a9eb3c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166ea56a9320d329637d524639a1cac5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a79dfc18bb9f311e8c72",
+    "url": "/static/css/7.5104ee78.chunk.css"
+  },
+  {
+    "revision": "8922d76ff4671c15cb4a",
+    "url": "/static/css/main.59ae3c47.chunk.css"
+  },
+  {
+    "revision": "73561973edd23b425b40",
+    "url": "/static/js/0.02d1d4a1.chunk.js"
+  },
+  {
+    "revision": "742569f3e998307970e9",
+    "url": "/static/js/1.eef3919e.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/static/js/1.eef3919e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d7076d842a5e08e121",
+    "url": "/static/js/4.2dc9018a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/4.2dc9018a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e702a9357d157d0edfa7",
+    "url": "/static/js/5.5ab91621.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/5.5ab91621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73fb9e4d88e45884eeb",
+    "url": "/static/js/6.9844f64c.chunk.js"
+  },
+  {
+    "revision": "a79dfc18bb9f311e8c72",
+    "url": "/static/js/7.07ba5bde.chunk.js"
+  },
+  {
+    "revision": "863f5b1c5fe0d425eb9c",
+    "url": "/static/js/8.4dbfb03e.chunk.js"
+  },
+  {
+    "revision": "a4cc12fb1462bc056baa",
+    "url": "/static/js/9.2a12d1af.chunk.js"
+  },
+  {
+    "revision": "b1c988d5f804f31d378acda419bf3e59",
+    "url": "/static/js/9.2a12d1af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8922d76ff4671c15cb4a",
+    "url": "/static/js/main.fe24d594.chunk.js"
+  },
+  {
+    "revision": "ea4029e00f5cd045590b",
+    "url": "/static/js/runtime-main.35601ab5.js"
+  },
+  {
+    "revision": "3055674f97ef1b295ba52ee8c457a71a",
+    "url": "/static/media/flowplayer.3055674f.woff"
+  },
+  {
+    "revision": "73ccb97fd8df0703038a40b00dc8ae5f",
+    "url": "/static/media/flowplayer.73ccb97f.woff2"
+  },
+  {
+    "revision": "8b1c59b76bd680bb3bf1b759d32c9b35",
+    "url": "/static/media/flowplayer.8b1c59b7.eot"
+  }
+]);
